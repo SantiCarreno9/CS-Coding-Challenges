9e79220a508fb33b7e2a93caca483ba6b506fbf1
Santiago Felipe Carreño <santiago.carreno05@gmail.com>
2025-12-16 13:34:49 -0500

    RemoveDuplicates exercise added

---
+++ CS-Coding-Exercises-Test/LeetCodeTest/RemoveDuplicatesTest.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode;
+
+namespace LeetCodeTest.AddTwoNumbersTest;
+
+[TestFixture]
+public class RemoveDuplicatesTest
+{
+    [TestCase(new int[] { 1, 1, 2 }, new int[] { 1, 2, 2 }, 2)]
+    [TestCase(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, new int[] { 0, 1, 2, 3, 4 }, 5)]
+    public void Test(int[] nums, int[] expectedNums, int k)
+    {
+        Assert.That(RemoveDuplicates.RemoveDuplicatesInSortedArray(nums), Is.EqualTo(k));
+        for (int i = 0; i < k; i++)
+        {
+            Assert.That(nums[i], Is.EqualTo(expectedNums[i]));
+        }
+    }
+}
